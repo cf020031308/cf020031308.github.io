d1031810d56d14c181153708c079aeacf3b01002
Roy <cf020031308@163.com>
2019-03-28 17:09:23 +0800

    bugfix

--- dotfiles/termux/clipboard2instapaper.sh
+++ dotfiles/termux/clipboard2instapaper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-termux-vibrate -d 10 && termux-clipboard-get | xargs -I{url} curl -u `cat ~/.accounts/instapaper` "https://www.instapaper.com/api/add?url={url}" -vv 2>&1 | grep "HTTP|Content-Location|X-Instapaper-Title" | termux-toast
+termux-vibrate -d 10 && termux-clipboard-get | xargs -I{url} curl -u `cat ~/.accounts/instapaper` "https://www.instapaper.com/api/add?url={url}" -vv 2>&1 | grep -E "HTTP|Content-Location|X-Instapaper-Title" | termux-toast
