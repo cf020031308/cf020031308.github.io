27e4db39f3285f3c7f1bb98da2631d8ebf7aac18
Roy <cf020031308@163.com>
2019-05-31 19:25:29 +0800

    Close #21: Update feeds with the use of pup

--- dotfiles/newsbeuter/feeds/uestc_yz.sh
+++ dotfiles/newsbeuter/feeds/uestc_yz.sh
@@ -1,25 +1,17 @@
 #!/bin/bash -
 
-utctime=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-tpl=$(cat <<AWK
-BEGIN{print "\
-<?xml version='1.0' encoding='utf-8'?>\
-<feed xmlns='http://www.w3.org/2005/Atom'>\
-  <title> 电子科大研究生招生网 </title>\
-  <id> uestc:recuite </id>\
-  <updated>$utctime</updated>\
-"}
-{printf "\
-  <entry>\
-    <title>%s</title>\
-    <id>%s</id>\
-    <published>$utctime</published>\
-    <link href=\"%s\">%s</link>\
-    <author><name> UESTC </name></author>\
-  </entry>\
-", \$2, \$1, \$1, \$1}
-END{prnit "</feed>"}
-AWK
-)
-curl "https://yz.uestc.edu.cn/shuoshizhaosheng/" 2>&- | xmllint 2>&- --html --xpath '//div[@id="mcontent"]//table[@class="box"][1]//li/a/@*' - | sed 's/ href="/\
-/g' | sed -e '1d' -e 's/" title="/ /' -e 's/"$//' | awk "$tpl"
+echo "<?xml version='1.0' encoding='utf-8'?>
+<feed xmlns='http://www.w3.org/2005/Atom'>
+  <title> 电子科大研究生招生网 </title>
+  <id> uestc:recuite </id>
+  <updated>$(date -u +"%Y-%m-%dT%H:%M:%SZ")</updated>"
+curl "https://yz.uestc.edu.cn/shuoshizhaosheng/" 2>&- | pup 'table.box li:parent-of(span) json{}' | jq -r '.[] as $li | "
+<entry>
+  <title>\($li.children[0].title)</title>
+  <id>\($li.children[0].href)</id>
+  <published>\($li.children[1].text)</published>
+  <link href=\"\($li.children[0].href)\">\($li.children[0].href)</link>
+  <author><name> UESTC </name></author>
+</entry>"' | sed 's;<link href="/;<link href="https://yz.uestc.edu.cn/;'
+echo ""
+echo "</feed>"
