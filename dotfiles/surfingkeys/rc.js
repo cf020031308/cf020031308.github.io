c82d6e83c6a548a479b9b19edf4e81001340b0d9
Roy <cf020031308@163.com>
2020-07-09 17:02:05 +0800

    feat(dotfiles): replace cvim with SurfingKeys

---
+++ dotfiles/surfingkeys/rc.js
@@ -0,0 +1,9 @@
+addSearchAlias('zh', 'zhihu', 'https://www.zhihu.com/search?type=content&sort=upvote&q=');
+addSearchAlias('bk', 'douban book', 'http://book.douban.com/subject_search?cat=1001&search_text=');
+addSearchAlias('mv', 'douban movie', 'http://movie.douban.com/subject_search?cat=1002&search_text=');
+addSearchAlias('cn', 'translate to zh-CN', 'http://translate.google.cn/#auto/zh-CN/');
+addSearchAlias('en', 'translate to en', 'http://translate.google.cn/#auto/en/');
+addSearchAlias('gh', 'github', 'https://github.com/search?o=desc&s=stars&type=Repositories&utf8=%E2%9C%93&q=');
+addSearchAlias('so', 'stackoverflow', 'http://stackoverflow.com/search?q=');
+addSearchAlias('tb', 'taobao', 'http://s.taobao.com/search?oe=utf-8&f=8&q=');
+addSearchAlias('gs', 'google scholar', 'https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&btnG=&q=');
