79f56890915ad3aff98c43abf26c1946765f7119
Roy <cf020031308@163.com>
2025-03-09 20:49:37 +0800

    feat: update dotfiles for jrnl

---
+++ dotfiles/jrnl/bootstrap.sh
@@ -0,0 +1,5 @@
+pip install jrnl
+mkdir -p ~/.local/share/jrnl
+ln -sf ~/Nutstore/journal.txt ~/.local/share/jrnl/journal.txt
+mkdir -p ~/.config/jrnl
+ln -sf $PWD/jrnl.yaml ~/.config/jrnl/jrnl.yaml
